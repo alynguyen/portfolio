83b62b7d0b3937f791655c5844b4670034deb2ba
Aly Nguyen <alynguyen@Alys-MacBook-Pro-2.local>
2019-10-23 19:18:16 -0700

    Updates

---
+++ precache-manifest.e0d31c87aaf7776e49af3bf3ddb7560f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a9fbb970b3b35756b82de307270560",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e9af51d94de5df8399a",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "25256069cbc05f271817",
+    "url": "/portfolio/static/css/main.3aac4567.chunk.css"
+  },
+  {
+    "revision": "1e9af51d94de5df8399a",
+    "url": "/portfolio/static/js/2.b852ce4e.chunk.js"
+  },
+  {
+    "revision": "25256069cbc05f271817",
+    "url": "/portfolio/static/js/main.a1c58c45.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
